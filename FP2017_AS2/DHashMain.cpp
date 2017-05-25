13cc599b7a5acfbb8422f5d6e317014d17f69f79
R@CC00N <yiseo1011@gmail.com>
2017-05-25 19:06:29 +0900

    Read & Save sampleData
    but it occur fatal heap corruption

---
+++ FP2017_AS2/DHashMain.cpp
@@ -0,0 +1,93 @@
+#pragma warning(disable: 4996)
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+unsigned int LIST_SIZE;
+
+class StElement {
+	char name[20];
+	unsigned int studentID;
+	float score;
+	unsigned int advisorID;
+public:
+	// set class elements
+	void setName(char* _name) { strcpy( name, _name ); }
+	void setSID(unsigned int _studentID) { studentID = _studentID; }
+	void setScore(float _score) { score = _score; };
+	void setAID(unsigned int _advisorID) { advisorID = _advisorID; }
+
+	// get class elements
+	char* getName() { return name; }
+	unsigned int getSID() { return studentID; }
+	float getScore() { return score; }
+	unsigned int getAID() { return advisorID; }
+
+	// functions
+	void setElement(char* , unsigned int , float , unsigned int );
+};
+
+void StElement::setElement(char* _name, unsigned int _studentID, float _score, unsigned int _advisorID) {
+	setName(_name);
+	setSID(_studentID);
+	setScore(_score);
+	setAID(_advisorID);
+}
+
+StElement getToken(char* convStr) {
+	StElement bufElement;
+	char* tokList;
+
+	tokList = strtok(convStr, ",");
+	bufElement.setName(tokList);
+
+	tokList = strtok(NULL, ",");
+	bufElement.setSID(atoi(tokList));
+
+	tokList = strtok(NULL, ",");
+	bufElement.setScore(atof(tokList));
+
+	tokList = strtok(NULL, ",");
+	bufElement.setAID(atoi(tokList));
+
+	return bufElement;
+}
+
+int main() {
+	
+	ifstream is;
+	is.open("sampleData.csv");
+	if (is.is_open() == false){
+		cout << "File open Failed" << endl; return 1;
+	}
+
+	char* convStr;
+	string bufStr;
+	StElement bufElement;
+	vector<StElement> oData;
+
+	// get LIST_SIZE
+	getline(is, bufStr);
+	// fatal error : heap corruption
+	convStr = (char*)malloc(sizeof(bufStr)+1);
+	convStr[sizeof(bufStr)] = '\0';
+	strcpy(convStr, bufStr.c_str());
+	convStr = strtok(convStr, ",");
+	LIST_SIZE = atoi(convStr);
+
+	for (int i = 0; i < LIST_SIZE; i++) {
+		getline(is, bufStr);
+		strcpy(convStr, bufStr.c_str());
+		bufElement = getToken(convStr);
+		oData.push_back(bufElement);
+	}
+
+	free(convStr);
+	cout << "end";
+	is.close();
+	return 0;
+}
